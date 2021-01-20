0f691949d0a44986bfbf61cc9d9611cc2947471a
khizerdev <46632721+khizerdev@users.noreply.github.com>
2021-01-20 15:30:22 +0500

    Admin Template Integration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './layout/Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+        <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
